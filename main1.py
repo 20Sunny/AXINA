eec288d0f97fa93b73a33588aad3e8cf98a57c77
Sunny Sharma <119580558+20Sunny@users.noreply.github.com>
2024-12-08 21:56:36 +0530

    Browser Update

--- main1.py
+++ main1.py
@@ -1,9 +1,21 @@
 import sys
 import os
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import * 
-from PyQt5.QtWebEngineWidgets import * 
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import QUrl, QSize
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTabWidget,
+    QVBoxLayout,
+    QWidget,
+    QToolBar,
+    QLabel,
+    QAction,
+    QLineEdit,
+    QFileDialog,
+    QMessageBox,
+)
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineDownloadItem
+from PyQt5.QtGui import QIcon
 
 # HTML content for the custom home page
 HOME_PAGE_HTML = """
@@ -12,7 +24,7 @@ HOME_PAGE_HTML = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Axina Browser Home</title>
+    <title>Hi, What's Up</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap');
         body {
@@ -137,7 +149,7 @@ HOME_PAGE_HTML = """
         // Auto-update internet speed every 5 seconds
         setInterval(testInternetSpeed, 500);
         testInternetSpeed(); // Initial test
-
+//--------------------------------------------------------------------------------------------------------------------------
         // Search Form Functionality
         document.getElementById("searchForm").addEventListener("submit", function (e) {
             e.preventDefault();
@@ -148,68 +160,70 @@ HOME_PAGE_HTML = """
     </script>
 </body>
 </html>
-
 """
 
-# Save the home page to a local file
+# Save the home page HTML locally
 def save_home_page():
     with open("homepage.html", "w") as file:
         file.write(HOME_PAGE_HTML)
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
 
-        # Save the home page HTML locally
+        # Save the home page locally
         save_home_page()
 
-        # Set up the browser
-        self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl.fromLocalFile(os.path.abspath("homepage.html")))
-        self.browser.urlChanged.connect(self.update_url)
-        self.browser.titleChanged.connect(self.update_title)
-        self.browser.loadFinished.connect(self.update_status)
-
-        self.setCentralWidget(self.browser)
-        self.setWindowIcon(QIcon("browser_icon.png"))  # Add a custom icon if available
+        self.setWindowTitle("Axina Browser")
+        self.setWindowIcon(QIcon("browser_icon.png"))
         self.showMaximized()
 
-        # Set default zoom level to 120%
-        self.current_zoom = 1.2
-        self.browser.setZoomFactor(self.current_zoom)
+        # Create tab widget
+        self.tabs = QTabWidget()
+        self.tabs.setDocumentMode(True)
+        self.tabs.tabBarDoubleClicked.connect(lambda: self.add_new_tab())
+        self.tabs.currentChanged.connect(self.update_current_tab)
+        self.tabs.setTabsClosable(True)
+        self.tabs.tabCloseRequested.connect(self.close_current_tab)
 
-        # Create Navbar
+        self.setCentralWidget(self.tabs)
+
+        # Add navigation bar
         navbar = QToolBar("Navigation")
         navbar.setIconSize(QSize(20, 20))
         self.addToolBar(navbar)
 
-        # Back Button with Icon
+        # Back button
         back_btn = QAction(QIcon("left.png"), "Back", self)
-        back_btn.triggered.connect(self.browser.back)
+        back_btn.triggered.connect(self.navigate_back)
         navbar.addAction(back_btn)
 
-        # Forward Button with Icon
+        # Forward button
         forward_btn = QAction(QIcon("right.png"), "Forward", self)
-        forward_btn.triggered.connect(self.browser.forward)
+        forward_btn.triggered.connect(self.navigate_forward)
         navbar.addAction(forward_btn)
 
-        # Refresh Button with Icon
-        refresh_btn = QAction(QIcon("refresh.png"), "Refresh", self)
-        refresh_btn.triggered.connect(self.browser.reload)
-        navbar.addAction(refresh_btn)
+        # Reload button
+        reload_btn = QAction(QIcon("refresh.png"), "Reload", self)
+        reload_btn.triggered.connect(self.reload_page)
+        navbar.addAction(reload_btn)
 
-        # Home Button with Icon
+        # Home button
         home_btn = QAction(QIcon("home.png"), "Home", self)
         home_btn.triggered.connect(self.navigate_home)
         navbar.addAction(home_btn)
 
-        # URL Bar
+        # New Tab button
+        new_tab_btn = QAction(QIcon("new_tab.png"), "New Tab", self)
+        new_tab_btn.triggered.connect(lambda: self.add_new_tab())
+        navbar.addAction(new_tab_btn)
+
+        # URL bar
         self.url_bar = QLineEdit()
-        self.url_bar.setPlaceholderText("Enter URL or search...")
         self.url_bar.returnPressed.connect(self.navigate_to_url)
         navbar.addWidget(self.url_bar)
 
-        # Zoom In Button
+        # Zoom In button
         zoom_in_btn = QAction(QIcon("plus.png"), "Zoom In", self)
         zoom_in_btn.triggered.connect(self.zoom_in)
         navbar.addAction(zoom_in_btn)
@@ -218,59 +232,99 @@ class MainWindow(QMainWindow):
         self.zoom_label = QLabel("100%")
         navbar.addWidget(self.zoom_label)
 
-        # Zoom Out Button
+        # Zoom Out button
         zoom_out_btn = QAction(QIcon("minus.png"), "Zoom Out", self)
         zoom_out_btn.triggered.connect(self.zoom_out)
         navbar.addAction(zoom_out_btn)
 
-        # Status Bar
-        self.status_bar = QStatusBar()
-        self.setStatusBar(self.status_bar)
 
-    def navigate_home(self):
-        # Load the custom home page
-        home_path = os.path.abspath("homepage.html")
-        self.browser.setUrl(QUrl.fromLocalFile(home_path))
+        # Add first tab
+        self.add_new_tab(QUrl.fromLocalFile(os.path.abspath("homepage.html")), "Home")
+
+        # Current zoom level
+        self.current_zoom_level = 1.2
+
+    def add_new_tab(self, qurl=None, label="New Tab"):
+        if qurl is None:
+            qurl = QUrl.fromLocalFile(os.path.abspath("homepage.html"))
+
+        browser = QWebEngineView()
+        browser.setUrl(qurl)
+        browser.page().profile().downloadRequested.connect(self.handle_download)
+
+        i = self.tabs.addTab(browser, label)
+        self.tabs.setCurrentIndex(i)
+
+        browser.urlChanged.connect(lambda qurl, browser=browser: self.update_url(qurl, browser))
+        browser.loadFinished.connect(lambda _, i=i, browser=browser: self.update_tab_title(i, browser))
+
+    def update_tab_title(self, i, browser):
+        title = browser.page().title()
+        self.tabs.setTabText(i, title)
+
+    def update_url(self, qurl, browser):
+        if browser == self.tabs.currentWidget():
+            self.url_bar.setText(qurl.toString())
 
     def navigate_to_url(self):
-        user_input = self.url_bar.text()
-        # Check if input looks like a URL
-        if "." in user_input and not user_input.startswith("http"):
-            user_input = "http://" + user_input
-        elif not user_input.startswith("http") and not "." in user_input:
-            # Treat as a Google search query
-            user_input = f"https://www.google.com/search?q={user_input}"
-        self.browser.setUrl(QUrl(user_input))
+        url = self.url_bar.text()
+        if not url.startswith("http"):
+            url = "http://" + url
+        self.tabs.currentWidget().setUrl(QUrl(url))
 
-    def update_url(self, q):
-        self.url_bar.setText(q.toString())
+    def navigate_home(self):
+        self.tabs.currentWidget().setUrl(QUrl.fromLocalFile(os.path.abspath("homepage.html")))
 
-    def update_title(self, title):
-        self.setWindowTitle(title + " - Axina")
+    def navigate_back(self):
+        self.tabs.currentWidget().back()
 
-    def update_status(self):
-        self.status_bar.showMessage("Page Loaded", 2000)  # Message stays for 2 seconds
+    def navigate_forward(self):
+        self.tabs.currentWidget().forward()
 
+    def reload_page(self):
+        self.tabs.currentWidget().reload()
+
+    def update_current_tab(self, i):
+        if i >= 0:
+            browser = self.tabs.widget(i)
+            self.update_url(browser.url(), browser)
+
+    def close_current_tab(self, i):
+        if self.tabs.count() > 1:
+            self.tabs.removeTab(i)
+
+    def handle_download(self, download_item: QWebEngineDownloadItem):
+        original_filename = os.path.basename(download_item.url().toString())
+        save_path, _ = QFileDialog.getSaveFileName(self, "Save File", original_filename)
+        if save_path:
+            download_item.setPath(save_path)
+            download_item.accept()
+            download_item.finished.connect(
+                lambda: QMessageBox.information(self, "Download Complete", f"File downloaded to: {save_path}")
+            )
+        else:
+            download_item.cancel()
+
+    # Zoom In function
     def zoom_in(self):
-        self.current_zoom += 0.1
-        self.browser.setZoomFactor(self.current_zoom)
+        self.current_zoom_level += 0.1
+        self.tabs.currentWidget().setZoomFactor(self.current_zoom_level)
         self.update_zoom_label()
 
+    # Zoom Out function
     def zoom_out(self):
-        self.current_zoom -= 0.1
-        if self.current_zoom < 0.5:  # Minimum zoom level
-            self.current_zoom = 0.5
-        self.browser.setZoomFactor(self.current_zoom)
+        self.current_zoom_level -= 0.1
+        self.tabs.currentWidget().setZoomFactor(self.current_zoom_level)
         self.update_zoom_label()
 
+    # Update Zoom Label
     def update_zoom_label(self):
-        zoom_percentage = int(self.current_zoom * 100)
+        zoom_percentage = int((self.current_zoom_level - 0.2) * 100)
         self.zoom_label.setText(f"{zoom_percentage}%")
-        self.status_bar.showMessage(f"{zoom_percentage}%", 2000)
 
 # Application Entry Point
 app = QApplication(sys.argv)
-QApplication.setApplicationName('Axina Browser')
+QApplication.setApplicationName("Axina Browser")
 
 window = MainWindow()
 app.exec_()
